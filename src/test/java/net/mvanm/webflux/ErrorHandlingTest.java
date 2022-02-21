eb81ceb6e26a14d705d75f64d7ac422e82686503
MarcMeer <6929748+MarcMeer@users.noreply.github.com>
2022-02-21 21:05:55 +0100

    onErrorContinue voorbeeld toegevoegd

--- src/test/java/net/mvanm/webflux/ErrorHandlingTest.java
+++ src/test/java/net/mvanm/webflux/ErrorHandlingTest.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Slf4j
 public class ErrorHandlingTest {
@@ -26,6 +29,29 @@ public class ErrorHandlingTest {
         });
 
     }
+
+    @Test
+    void flux_met_een_fout() {
+        Flux<Integer> integerFlux = Flux.fromIterable(List.of("1", "2", "3", "4"))
+                .map(this::errorWanneerEen)
+                .doOnNext(s -> log.info("Waarde door stream {}", s))
+                // Met onErrorContinue loggen we de fout en bij behorende waarde, stream zal de andere waardes wel netjes
+                // afhandelen. Dit kan handig zijn als je met streams werkt waarbij bij een exception niet de gehele stream
+                // moet falen. Indien je onderstaande regel uitzet, zal er geen enkele waarde nog naar de subscribere worden
+                // gezet maar de stream een exception gooien.
+                .onErrorContinue((throwable, o) -> log.error("Exception {} bij waarde {}",throwable.getMessage(), o.toString()))
+                .map(Integer::valueOf);
+
+        integerFlux.subscribe(i->log.info("Subscriber krijgt {}", i.toString()));
+    }
+
+    private String errorWanneerEen(String s) {
+        if(s.equals("1")) {
+            throw new IllegalStateException("Ik wil geen 1!");
+        }
+        return s;
+    }
+
 
     private static String creerFout(String s) {
         throw new IllegalStateException("Dit mag dus niet gebeuren!");
