cafec9c5e69a2f4ee73a0d9e4a40e1b42078ac69
MarcMeer <6929748+MarcMeer@users.noreply.github.com>
2022-02-21 19:34:04 +0100

    een lege test toegevoegd voor demo doeleinde

---
+++ src/test/java/net/mvanm/webflux/EmptyTest.java
@@ -0,0 +1,12 @@
+package net.mvanm.webflux;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class EmptyTest {
+    @Test
+    void test_something() {
+
+    }
+}
