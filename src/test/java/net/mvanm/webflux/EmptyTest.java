baddbb29cea141e0f49071ecc081e50c315fcd0e
MarcMeer <6929748+MarcMeer@users.noreply.github.com>
2022-02-23 19:12:31 +0100

    Logging op info dat is handiger tijdens de demo

--- src/test/java/net/mvanm/webflux/EmptyTest.java
+++ src/test/java/net/mvanm/webflux/EmptyTest.java
@@ -1,12 +1,23 @@
 package net.mvanm.webflux;
 
+import ch.qos.logback.classic.Level;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Slf4j
 public class EmptyTest {
-    @Test
-    void test_something() {
 
+    @BeforeEach
+    void loglevel() {
+        ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        rootLogger.setLevel(Level.INFO);
+    }
+
+    @Test
+    void test_something() throws InterruptedException {
     }
 }
